62aa86f52592295728df85156a576cf9611c5d54
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2019-10-14 14:21:13 -0700

    Add files via upload

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,11 +20,12 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] jar; 
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
+		
 	}
 	
 	public void start() {
@@ -33,22 +34,28 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
+		String futureint = JOptionPane.showInputDialog(null, "Enter a positive number below");
+		int pos = Integer.parseInt(futureint);
 		
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		JButton[] jar = new JButton[pos];
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
+		for(int i=0;i<jar.length;i++){
+			jar[i].addActionListener(null);
+			panel.add(jar[i]);
+		}
 		
 		//9 add the panel to the window
-		
+		window.add(panel);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "Alright buckaroo here's the game - There will be "+pos+" buttons on the screen. All you gotta do is find them. Good Luck!");
 		//13. initialize the hiddenButton variable to a random number less than the int created int step 3
 		
 		//14. Set the text of the JButton located at hiddenButton the read "ME"
